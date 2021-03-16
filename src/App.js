e1310733d46bb0fcdb8ba8ad079adc24d4c9d363
Yesim Kebabci <eyesim@sabanciuniv.edu>
2021-03-16 10:45:15 +0300

    Tweet embedding

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
+import icon from './icon.svg';
 import './App.css';
+import { useState } from 'react';
+import TweetDisplay from './TweetDisplay.js'
+import TrendChart from './TrendChart.js'
+
 
 function App() {
+  
+  const [newicon,setNewIcon] = useState(icon)
+
+const ButtonHandler = (logo) => {
+  console.log("Hello iz clicked")
+  // setName(newname)
+  setNewIcon(logo)
+
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <h2> TWITTER TRENDS</h2>
+      <img src={newicon} className="App-logo" alt="logo" /> 
+      <TrendChart></TrendChart>     
+
+      <h1 className="App-header">
+      <TweetDisplay></TweetDisplay>
+        <button onClick =  { () => ButtonHandler(logo)}>Click to see </button>
+         <p>
+          DO NOT edit <code>src/App.js</code> and save to reload.
         </p>
+   
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome
         </a>
-      </header>
+      </h1> 
     </div>
   );
 }
---
+++ src/PositiveTweets.js
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import TweetEmbed from 'react-tweet-embed'
+
+function PositiveTweets() {
+
+    useState[tweets, setTweets] = useState()
+    
+    return(
+    <div className = "BorderPosition">
+    
+          {/* <TweetChart title="positive" tweets="p" />
+          <TweetChart title="negative" tweets="n" /> */}
+            <TweetEmbed id = "1370338169493815296"/>
+    
+          </div>
+    
+    )
+    }
+    
+    export default PositiveTweets;
